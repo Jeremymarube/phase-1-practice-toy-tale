5157d0efcd2cbc0768cb8e283567d21fcecf20a3
Jeremy Marube <jeremymarube1@gmail.com>
2025-06-22 22:09:08 +0300

    practice toy-tale

--- src/index.js
+++ src/index.js
@@ -13,3 +13,81 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+document.addEventListener("DOMContentLoaded", () => {
+  fetchToys();
+  setupFormListener();
+});
+
+function fetchToys() {
+  fetch('http://localhost:3000/toys')
+    .then(response => response.json())
+    .then(toys => {
+      toys.forEach(renderToyCard);
+    });
+}
+function renderToyCard(toy) {
+  const toyCollection = document.getElementById('toy-collection');
+
+  const card = document.createElement('div');
+  card.className = 'card';
+
+  card.innerHTML = `
+    <h2>${toy.name}</h2>
+    <img src="${toy.image}" class="toy-avatar" />
+    <p>${toy.likes} Likes</p>
+    <button class="like-btn" id="${toy.id}">Like ❤️</button>
+  `;
+
+  card.querySelector('.like-btn').addEventListener('click', () => {
+    likeToy(toy, card);
+  });
+
+  toyCollection.appendChild(card);
+}
+function setupFormListener() {
+  const form = document.querySelector('.add-toy-form');
+  form.addEventListener('submit', handleToySubmit);
+}
+
+function handleToySubmit(e) {
+  e.preventDefault();
+  const name = e.target.name.value;
+  const image = e.target.image.value;
+
+  const newToy = {
+    name,
+    image,
+    likes: 0
+  };
+
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(newToy)
+  })
+    .then(response => response.json())
+    .then(toy => {
+      renderToyCard(toy);
+      e.target.reset(); // reset the form
+    });
+}
+function likeToy(toy, card) {
+  const newLikes = toy.likes + 1;
+
+  fetch(`http://localhost:3000/toys/${toy.id}`, {
+    method: 'PATCH',
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({ likes: newLikes })
+  })
+    .then(response => response.json())
+    .then(updatedToy => {
+      toy.likes = updatedToy.likes;
+      card.querySelector('p').textContent = `${updatedToy.likes} Likes`;
+    });
+}
